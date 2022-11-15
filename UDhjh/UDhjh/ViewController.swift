1cfc759fba53eaf747262c3108684de11aea80ec
Jaeha Hwang <wogk216@naver.com>
2022-11-15 23:04:44 +0900

    0부터 1씩 증감할 수 있는 미니 앱

---
+++ UDhjh/UDhjh/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  UDhjh
+//
+//  Created by 황재하 on 11/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var lblNumber: UILabel!
+    var x = 0
+    
+    @IBAction func btnUp(_ sender: UIButton) {
+        x = x + 1
+        lblNumber.text = String(x)
+    }
+    
+    
+    @IBAction func btnDown(_ sender: UIButton) {
+        x = x - 1
+        lblNumber.text = String(x)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
