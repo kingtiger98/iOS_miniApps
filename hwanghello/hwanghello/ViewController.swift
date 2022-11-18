5f16fc8b08a538628c49f9117eaeb609d47b085d
Jaeha Hwang <wogk216@naver.com>
2022-11-18 14:36:13 +0900

    Mini app : 이름을 출력하는 앱

--- hwanghello/hwanghello/ViewController.swift
+++ hwanghello/hwanghello/ViewController.swift
@@ -1,17 +1,31 @@
-//
 //  ViewController.swift
 //  hwanghello
-//
 //  Created by 황재하 on 11/8/22.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var txtName: UITextField!
     @IBOutlet weak var lblHello: UILabel!
+    
+    // Delete버튼 구현방법_1
+    // action버튼 => btnDelete 삭제 버튼 생성
+    //    @IBAction func btnDelete(_ sender: UIButton) {
+    //        lblHello.text = ""
+    //        txtName.text = ""
+    //    }
+    
+    // Delete버튼 구현방법_2
+    // action버튼 => btnSend에 태그를 1에' 누르세요" 버튼,
+    // tag2에 "Delete" 버튼 할당
     @IBAction func btnSend(_ sender: UIButton) {
-        lblHello.text = "Hello, " + txtName.text!
+        if sender.tag == 1 {
+            lblHello.text = "Hello, " + txtName.text!
+            lblHello.textColor = UIColor.white
+        } else if sender.tag == 2 {
+            lblHello.text = ""
+            txtName.text = ""
+        }
     }
     
     override func viewDidLoad() {
