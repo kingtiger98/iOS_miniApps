63d58cdd999c409414e08815f1987344556ef590
Jaeha Hwang <wogk216@naver.com>
2022-11-18 16:49:23 +0900

    Mini app : 숫자를 1씩 증감시키는 계산기 앱

--- UDhjh/UDhjh/ViewController.swift
+++ UDhjh/UDhjh/ViewController.swift
@@ -1,16 +1,9 @@
-//
 //  ViewController.swift
 //  UDhjh
-//
 //  Created by 황재하 on 11/15/22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    
-    
     @IBOutlet weak var lblNumber: UILabel!
     var x = 0
     
@@ -19,18 +12,13 @@ class ViewController: UIViewController {
         lblNumber.text = String(x)
     }
     
-    
     @IBAction func btnDown(_ sender: UIButton) {
         x = x - 1
         lblNumber.text = String(x)
     }
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
