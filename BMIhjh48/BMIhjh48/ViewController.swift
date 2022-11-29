a462a015b92654e50c84323f49d1a9896f93f6c6
Jaeha Hwang <wogk216@naver.com>
2022-11-29 14:08:20 +0900

    BMI계산기 미니앱

---
+++ BMIhjh48/BMIhjh48/ViewController.swift
@@ -0,0 +1,13 @@
+//  ViewController.swift
+//  BMIhjh48
+//  Created by 황재하 on 11/22/22.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
